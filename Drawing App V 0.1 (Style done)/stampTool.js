7b9a3f288d03db1ee1909fe3522d462ad860ea1a
dstor0511 <105460494+dstor0511@users.noreply.github.com>
2022-07-14 18:57:16 -0500

    Attempt to create slider only when the function was selected
    Attempt to create slider only when the function was selected. Failed.

--- Drawing App V 0.1 (Style done)/stampTool.js
+++ Drawing App V 0.1 (Style done)/stampTool.js
@@ -2,17 +2,26 @@ function StampTool (){
     // set an icon and name for the constructor function
     this.icon = "assets/sticker.png";
     this.name = "stamp";
+    this.stickerSliderSize = null;
 
-    var stickerSliderSize = createSlider(5, 50, 20);
-    var sticker = loadImage("assets/sticker.png");
+    // var stickerSliderSize = createSlider(5, 50, 20);
+    this.sticker = loadImage("assets/sticker.png");
 
     this.sliders = function(){
-        stickerSliderSize.parent(select(".options"))
+        // stickerSliderSize.parent(select(".options"))
+        select(".options").html("<input type='range' min='5' max='50' value='20' class='slider' id='stickerSliderSize'>")
 
+        // slider controller
+        select("#stickerSliderSize").mouseClicked(function(){
+            stickerSliderSize = select("#" + this.elt.id);
+            console.log(24);
+
+        })
     };
 
     this.draw = function(){
-       if (mouseIsPressed){
+       
+        if (mouseIsPressed){
             for (var i = 0; i < stickerSliderSize.value(); i ++)
             {
                 var stickerSize = stickerSliderSize.value();
